04d4181871173e4aebb2a491b3347f08042b3d28
Benjamin Duran <bduran@netflix.com>
2024-04-23 13:09:24 -0700

    fix: fixed untarring on macos platforms

--- src/extractNodeArchive.mjs
+++ src/extractNodeArchive.mjs
@@ -1,12 +1,39 @@
-import decompress from 'decompress';
-import decompressTarxz from 'decompress-tarxz';
+import zlib from 'node:zlib';
+
 import extractZip from 'extract-zip';
 import glob from 'fast-glob';
 import fs from 'fs-extra';
+import lzma from 'lzma-native';
 import path from 'path';
 import * as tar from 'tar';
 
 import { getTempFolder } from './getTempFolder.mjs';
+
+/**
+ * Generically inflates an archive file
+ * @param {string} archivePath
+ * @param {string} dest
+ * @returns {Promise<void>}
+ */
+async function extractArchive(archivePath, dest) {
+  if (archivePath.endsWith('.zip')) return extractZip(archivePath, { dir: dest });
+  return new Promise((resolve, reject) => {
+    const readStream = fs.createReadStream(archivePath);
+    readStream.once('error', reject);
+    if (archivePath.endsWith('.xz')) {
+      readStream
+        .pipe(lzma.createDecompressor())
+        .pipe(tar.extract({ C: dest }))
+        .once('error', reject)
+        .once('finish', resolve);
+    } else
+      readStream
+        .pipe(zlib.createGunzip())
+        .pipe(tar.extract({ C: dest }))
+        .once('error', reject)
+        .once('finish', resolve);
+  });
+}
 
 /**
  * Given a path to a downloaded archive, extracts all of the contents
@@ -19,12 +46,7 @@ export async function extractNodeArchive(archivePath) {
 
   console.info(`Extracting ${archivePath} to ${dest}`);
 
-  if (archivePath.endsWith('.zip')) await extractZip(archivePath, { dir: dest });
-  else if (archivePath.endsWith('.xz')) {
-    await decompress(archivePath, dest, {
-      plugins: [decompressTarxz()],
-    });
-  } else await tar.x({ file: archivePath, C: dest });
+  await extractArchive(archivePath, dest);
 
   const [nixexe = ''] = await glob(path.join(dest, '**', 'bin', 'node'), { absolute: true, onlyFiles: true });
   const [winexec = ''] = await glob(path.join(dest, '**', 'node.exe'), { absolute: true, onlyFiles: true });
